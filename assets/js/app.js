ca834fd8cc566e9375952cb758d920318f8b3d0f
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2023-09-04 12:22:07 +0300

    #5656

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,72 @@
 let lightThemeBtn = document.getElementById("lightTheme");
 let darkThemeBtn = document.getElementById("darkTheme");
 let body = document.querySelector("body");
-let brand = document.querySelector(".brand");
+let brand = document.querySelectorAll(".brand");
+let nami = document.querySelector("#nami");
 lightThemeBtn.addEventListener("click", () => {
   body.classList.remove("dark-theme");
-  brand.src = "assets/images/logo.svg";
-  document.querySelector(".illustration img").src =
-    "assets/images/forgetpass1.svg";
-  document.querySelector(".otp img").src = "assets/images/otp1.svg";
-  document.querySelector(".confirmpass img").src = "assets/images/newpass1.svg";
+  brand.forEach(b => {
+    b.src = "assets/images/logo.svg";
+  });
+  nami.src = "assets/images/nami.svg";
+  // document.querySelector(".illustration img").src =
+  //   "assets/images/forgetpass1.svg";
+  // document.querySelector(".otp img").src = "assets/images/otp1.svg";
+  // document.querySelector(".confirmpass img").src = "assets/images/newpass1.svg";
 });
 darkThemeBtn.addEventListener("click", () => {
   body.classList.add("dark-theme");
-  brand.src = "assets/images/logo-dark.svg";
-  document.querySelector(".illustration img").src =
-    "assets/images/forgetpass2.svg";
-  document.querySelector(".otp img").src = "assets/images/otp2.svg";
-  document.querySelector(".confirmpass img").src = "assets/images/newpass2.svg";
+  brand.forEach(b => {
+    b.src = "assets/images/logo-dark.svg";
+  });
+  nami.src = "assets/images/nami2.svg";
+  // document.querySelector(".illustration img").src =
+  //   "assets/images/forgetpass2.svg";
+  // document.querySelector(".otp img").src = "assets/images/otp2.svg";
+  // document.querySelector(".confirmpass img").src = "assets/images/newpass2.svg";
+});
+$(document).ready(function() {
+  $(".service-card h6").each(function() {
+    var text = $(this).text();
+    if (text.length > 70) {
+      var truncatedText =
+        $.trim(text).substring(0, 70).split(" ").slice(0, -1).join(" ") + "...";
+      $(this).text(truncatedText);
+    }
+  });
+});
+//hero swiper
+var swiper = new Swiper(".heroSwiper", {
+  direction: "vertical",
+  effect: "fade",
+  speed: 1000,
+  pagination: {
+    el: ".mainSliderPagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".mainSliderNext",
+    prevEl: ".mainSliderPrev"
+  }
+});
+//services swiper
+var categoriesSwiper = new Swiper(".categories-swiper", {
+  loop: true,
+  spaceBetween: 30,
+  speed: 2000,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 4
+    },
+    768: {
+      slidesPerView: 3
+    },
+    350: {
+      slidesPerView: 1
+    }
+  }
 });
